705a8694588f9d2c1bf3ea04a2f265112807d8e8
Khaled Hosny <khaledhosny@eglug.org>
2009-12-25 18:00:37 +0200

    A dumb way to re-generate the font without closing the viewer

--- scripts/font_preview.py
+++ scripts/font_preview.py
@@ -1,13 +1,18 @@
 import tempfile
 import subprocess
+import os
 
 viewer = "fv"
 
-def Preview(data, font):
-	otf = tempfile.NamedTemporaryFile(suffix=".otf", delete=False).name
+def Preview(re, font):
+	otf = "%s/%s.otf" %(tempfile.gettempdir(), font.fontname)
 	cmd = "%s %s" %(viewer, otf)
-	font.generate(otf)
-	subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	if re and os.path.isfile(otf):
+		font.generate(otf)
+	else:
+		font.generate(otf)
+		subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 
 fontforge.registerMenuItem(Preview, None, None, "Font", None, "Preview font")
+fontforge.registerMenuItem(Preview, None, True, "Font", None, "Re-preview font")
