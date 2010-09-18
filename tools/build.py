bc910e54029453a8a49477ceb7a4ce462b947a26
Khaled Hosny <khaledhosny@eglug.org>
2010-09-18 23:45:11 +0300

    Primitive kerning
    Very early kerning support, not very well tested.
    
    Kerning classes can't be used with RTL scripts (since I need to change
    both xoffset and advance width, but classes can only change the later),
    but kerning pairs are laborious, so I input kern classes and convert to
    kern pairs at build time.
    
    I'm considering to use contextual chaining positioning instead of kern
    pairs (should save space), but there is no direct visual feed back when
    editing single side bearing, I could generate it from  kern classes at
    build time, but FontForge scripting can edit/build contextual lookups,
    yet.

--- tools/build.py
+++ tools/build.py
@@ -80,6 +80,44 @@ def generate_css(font, out, base):
     file.write(css)
     file.close()
 
+def class2pair(font, remove):
+    """Looks for any kerning classes in the font and converts it to
+    RTL kerning pairs."""
+    kern_pairs = 0
+    kern_class = 0
+    new_subtable = ""
+    for lookup in font.gpos_lookups:
+        if font.getLookupInfo(lookup)[0] == "gpos_pair":
+            for subtable in font.getLookupSubtables(lookup):
+                if font.isKerningClass(subtable):
+                    kern_class += 1
+                    new_subtable = subtable + " pairs"
+                    font.addLookupSubtable(lookup, new_subtable)
+                    kclass   = font.getKerningClass(subtable)
+                    klasses1 = kclass[0]
+                    klasses2 = kclass[1]
+                    offsets  = kclass[2]
+
+                    for klass1 in klasses1:
+                        if klass1 != None:
+                            for name in klass1:
+                                glyph = font.createChar(-1, name)
+                                for klass2 in klasses2:
+                                    if klass2 != None:
+                                        kern = offsets[klasses1.index(klass1)
+                                                *len(klasses2)+
+                                                klasses2.index(klass2)]
+                                        if kern != 0:
+                                            for glyph2 in klass2:
+                                                glyph.addPosSub(new_subtable,
+                                                        glyph2,
+                                                        kern,0,kern,0,0,0,0,0)
+					        font.changed = True
+                                                kern_pairs  += 1
+                    if remove:
+                        font.removeLookupSubtable(subtable)
+    return new_subtable
+
 def main(sfd, out):
     css = False
     ext = os.path.splitext(out)[1].lower()
@@ -93,6 +131,7 @@ def main(sfd, out):
         generate_css(font, out, base)
     else:
         fake_marks(font)
+        class2pair(font, True)
         font.generate(out)
 
     font.close()
