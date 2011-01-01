6ee585eb2e08c32678672b6eaa27a0e08e00d9b7
Khaled Hosny <khaledhosny@eglug.org>
2011-01-01 15:27:20 +0200

    Tab -> 4 spaces

--- tools/build_base.py
+++ tools/build_base.py
@@ -1,42 +1,42 @@
 def buildAccented(data, glyph):
-	if len(glyph.getPosSub("'ccmp' Glyph Decomposition-1")) == 1:
-		ccmp = glyph.getPosSub("'ccmp' Glyph Decomposition-1")[0]
-		glyph.clear()
-		glyph.addReference(ccmp[2])
-		glyph.useRefsMetrics(ccmp[2])
+    if len(glyph.getPosSub("'ccmp' Glyph Decomposition-1")) == 1:
+        ccmp = glyph.getPosSub("'ccmp' Glyph Decomposition-1")[0]
+        glyph.clear()
+        glyph.addReference(ccmp[2])
+        glyph.useRefsMetrics(ccmp[2])
 
-		if len(ccmp) > 3:
-			glyph.appendAccent(name=ccmp[3])
+        if len(ccmp) > 3:
+            glyph.appendAccent(name=ccmp[3])
 
-		return 1
+        return 1
 
 def drawBbox(data, glyph):
-	if data:
-		bbox = glyph.boundingBox()
-		xmin = bbox[0]
-		ymin = bbox[1]
-		xmax = bbox[2]
-		ymax = bbox[3]
+    if data:
+        bbox = glyph.boundingBox()
+        xmin = bbox[0]
+        ymin = bbox[1]
+        xmax = bbox[2]
+        ymax = bbox[3]
 
-		pen  = glyph.glyphPen(replace=False)
+        pen  = glyph.glyphPen(replace=False)
 
-		pen.moveTo(xmin, ymin)
-		pen.lineTo(xmax, ymin)
-		pen.lineTo(xmax, ymax)
-		pen.lineTo(xmin, ymax)
-		pen.lineTo(xmin, ymin)
-		pen.closePath()
+        pen.moveTo(xmin, ymin)
+        pen.lineTo(xmax, ymin)
+        pen.lineTo(xmax, ymax)
+        pen.lineTo(xmin, ymax)
+        pen.lineTo(xmin, ymin)
+        pen.closePath()
 
-		pen = None
+        pen = None
 
-	if data == 1:
-		glyph.right_side_bearing = 0
-		glyph.left_side_bearing  = 0
+    if data == 1:
+        glyph.right_side_bearing = 0
+        glyph.left_side_bearing  = 0
 
 def buildBase(data, font):
-	selection = font.selection.byGlyphs
-	for g in selection:
-		drawBbox(buildAccented(data, g), g)
+    selection = font.selection.byGlyphs
+    for g in selection:
+        drawBbox(buildAccented(data, g), g)
 
 fontforge.registerMenuItem(buildBase,     None, None, "Font",  None, "Bulaq", "Base", "Build base glyphs")
 fontforge.registerMenuItem(buildAccented, None, None, "Glyph", None, "Bulaq", "Base", "Build glyph from 'ccmp'")
