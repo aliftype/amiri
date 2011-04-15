30e20ea1e751950ae3485a86f97f541202d45c0b
Khaled Hosny <khaledhosny@eglug.org>
2011-04-15 20:15:01 +0200

    Add CC0 license

--- tools/build.py
+++ tools/build.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python
+#
+# build.py - Amiri font build utility
+#
+# Written in 2010-2011 by Khaled Hosny <khaledhosny@eglug.org>
+#
+# To the extent possible under law, the author have dedicated all copyright
+# and related and neighboring rights to this software to the public domain
+# worldwide. This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along
+# with this software. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 import fontforge
 import sys
