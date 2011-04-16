02514e1e1ead85964f3a797e78db64551a5ee509
Khaled Hosny <khaledhosny@eglug.org>
2011-04-16 16:38:53 +0200

    An attempt to make the text in PDF copyable
    PDF does not store text string but, sort of, positioned glyph indices,
    and PDF readers depend on glyph names to do reverse glyph->character
    mapping to allow for text searching/copying. This does not work for
    Amiri since a character can be represented by several glyphs, e.g. أ
    will be an Alef and small Hamza as two glyphs, which would result in two
    characters using the aforementioned method, not only that but many of
    our glyphs has no corresponding character, so this results in garbage
    when copying text.
    
    PDF has an ActualText tag that can hold the original text
    representation, which goes around the limitations of glyph->character
    mapping. This tiny lua module works by traversing the luatex node list
    before doing OpenType layout, tagging each node with a unique identifier
    and it original character, after OpenType processing new glyph nodes
    will inherit attributes of its parent (e.g. if <x> is mapped to
    <x.base><x.dot>, both will have the same identifier and character
    attributes) then the code will insert ActualText tag around any nodes
    with the same identifier (i.e. around <x.base><x.dot>).
    
    The code is not general purpose enough as it does not account for
    ligatures since Amiri font does not have any.

---
+++ tex/context/amiri.lua
@@ -0,0 +1,86 @@
+amiri = { }
+
+local code_attr = attributes.private("amiricharcode")
+local id_attr   = attributes.private("amiricharid")
+
+local glyph     = node.id("glyph")
+local hlist     = node.id("hlist")
+local vlist     = node.id("vlist")
+
+local format    = string.format
+local ids       = fonts.hashes.identifiers
+
+local function is_amiri(font)
+    font = ids[font]
+    font = font and font.shared
+    font = font and font.rawdata
+    font = font and font.metadata
+    font = font and font.familyname
+    if font == "Amiri" then
+        return true
+    else
+        return false
+    end
+end
+
+local charid = 0
+
+function amiri.initialise(head)
+    for n in node.traverse(head) do
+        if n.id == glyph and is_amiri(n.font) then
+            charid = charid + 1
+            node.set_attribute(n, id_attr,   charid)
+            node.set_attribute(n, code_attr, n.char)
+        end
+    end
+    return head
+end
+
+local function tosixteen(n)
+    if not n then
+        return "<feff>"
+    else
+        if n < 0x10000 then
+            n = format("%04x",n)
+        else
+            n = format("%04x%04x",n/1024+0xD800,n%1024+0xDC00)
+        end
+    end
+    return format("<feff%s>", n)
+end
+
+local function new_actualtext(code)
+    local actualtext = node.new(node.id("whatsit"), 8)
+    actualtext.mode = 1
+    if code then
+        actualtext.data = format("/Span << /ActualText %s >> BDC", tosixteen(code))
+    else
+        actualtext.data = "EMC"
+    end
+    return actualtext
+end
+
+function amiri.finalise(head)
+    for n in node.traverse(head) do
+        if n.id == glyph and is_amiri(n.font) then
+            local id   = node.has_attribute(n, id_attr)
+            local code = node.has_attribute(n, code_attr)
+            if n.prev and n.prev.id == glyph and is_amiri(n.prev.font) and node.has_attribute(n.prev, id_attr) == id then
+            else
+                local b = new_actualtext(code)
+                node.insert_before(head, n, b)
+            end
+            if n.next and n.next.id == glyph and is_amiri(n.next.font) and node.has_attribute(n.next, id_attr) == id then
+            else
+                local e = new_actualtext()
+                node.insert_after(head, n, e)
+            end
+        elseif n.id == hlist or n.id == vlist then
+            amiri.finalise(n.list)
+        end
+    end
+    return head
+end
+
+nodes.tasks.appendaction("processors", "characters",  "amiri.initialise")
+nodes.tasks.appendaction("shipouts",   "finishers",   "amiri.finalise")
