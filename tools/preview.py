df176926bd59a1ec87e568e181654eedd039f8f0
Khaled Hosny <khaledhosny@eglug.org>
2011-08-03 22:15:46 +0200

    Cosmetics

--- tools/preview.py
+++ tools/preview.py
@@ -3,10 +3,9 @@ import tempfile
 import subprocess
 import os
 
-out = ""
-
 viewer = "fontview"
 flags  = ("opentype", "dummy-dsig", "round", "short-post")
+out    = ""
 
 def Preview(re, obj):
     if type(obj).__name__ == "font":
@@ -15,24 +14,23 @@ def Preview(re, obj):
         font = obj.font
 
     global out
+
     if not out:
         if font.layers[1].is_quadratic:
             out = tempfile.NamedTemporaryFile(suffix=".ttf").name
         else:
             out = tempfile.NamedTemporaryFile(suffix=".otf").name
 
-    font.generate(out,flags=flags)
+    font.generate(out, flags=flags)
 
     if not re:
         cmd = "%s %s" %(viewer, out)
         subprocess.Popen(cmd,
-            shell=True,
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT)
+                shell=True,
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT)
 
 
-fontforge.registerMenuItem(
-        Preview,None,None,("Font","Glyph"),"P","Preview","Preview font")
-fontforge.registerMenuItem(
-        Preview,None,True,("Font","Glyph"),"R","Preview","Re-view font")
+fontforge.registerMenuItem(Preview, None, None, ("Font", "Glyph"), "P", "Preview", "Preview font")
+fontforge.registerMenuItem(Preview, None, True, ("Font", "Glyph"), "R", "Preview", "Re-view font")
