400b702ec65b8dbae19b64ef7c39f23e4eb40f9f
Khaled Hosny <khaledhosny@eglug.org>
2019-12-04 04:29:14 +0200

    Use Apache 2.0 license for the build script
    Apparently CC0 is still problematic:
    https://twitter.com/d_s_corbett/status/1201682324896792577

--- tools/build.py
+++ tools/build.py
@@ -3,15 +3,19 @@
 #
 # build.py - Amiri font build utility
 #
-# Written in 2010-2012 by Khaled Hosny <khaledhosny@eglug.org>
+# Copyright 2010-2019 Khaled Hosny <khaledhosny@eglug.org>
 #
-# To the extent possible under law, the author have dedicated all copyright
-# and related and neighboring rights to this software to the public domain
-# worldwide. This software is distributed without any warranty.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# You should have received a copy of the CC0 Public Domain Dedication along
-# with this software. If not, see
-# <http://creativecommons.org/publicdomain/zero/1.0/>.
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from __future__ import print_function
 
